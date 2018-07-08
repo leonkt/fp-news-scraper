58797939de14c2c7061bbf075b34099517b7364b
leonkt <38256492+leonkt@users.noreply.github.com>
2018-07-07 17:02:18 -0700

    added more general xpaths + added itemloader

--- spiders/SlavCrawl.py
+++ spiders/SlavCrawl.py
@@ -1,28 +1,35 @@
 import scrapy
-import items
+from scrapy import ItemLoader
+from item import Article
 
 
 class help_fp(scrapy.Spider):
     name = 'help_fp'
-
+    """
+    Starts crawling at the base website to yield links to individual articles.
+    This restricts the spider's ability to crawl to other websites not of the
+    allowed domain.
+    """
     def __init__(self):
         super(help_fp, self).__init__(self)
         self.start_urls = ['http://www.foreignpolicy.com/']
-
+    """
+    Creates a generator that yields a Request object for each link on the front
+    page and trending. Individual articles are parsed later.
+    """
     def parse(self, response):
-        for info in response.xpath('//article[@class="article"]/'):
-            article = items.Article()
-            article.title = response.xpath('//head/title/text()').extract()
-            article.date = response.xpath('//div[@class="meta-data"]/time\
-                                          /@title').extract()
-            article.author = response.xpath('//address[@class="author-list"]\
-                                            /a[@class="author"]\
-                                            /text()').extract()
-            article.topic = response.xpath('//div[@class="the-tags"]\
-                                            /a/text()').extract()
-            yield article
-
         for link in response.xpath('//div[@class="excerpt-content\
                                     content-block"]/a[@class="hed-heading\
                                     -excerpt"]/@href').extract():
-            yield scrapy.Request(link, callback=self.parse)
+            yield scrapy.Request(link, callback=self.parse_indiv_articles())
+    """
+    Collects metadata for each of the articles and populates items generated
+    for each of the individual articles. Passed to pipeline to be arranged into
+    documents.
+    """
+    def parse_indiv_articles(self, response):
+        il = ItemLoader(item=Article(), response=response)
+        il.add_xpath('title', '//head/title/text()')
+        il.add_xpath('date', '//time/text()')
+        il.add_xpath('text', '//article/div/p/text()')
+        return il.load_item()
